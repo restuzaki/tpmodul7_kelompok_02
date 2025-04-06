50aac1ae9806ea471a94439697198d2f3e53db45
Reiluthfi <164453246+Reiluthfi@users.noreply.github.com>
2025-04-07 00:54:18 +0700

    Update Program.cs

--- tpmodul7_kelompok_02/Program.cs
+++ tpmodul7_kelompok_02/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa103022300112.ReadJSON();
+        Console.WriteLine();
+        KuliahMahasiswa103022300112.ReadJSON();
+    }
+}
