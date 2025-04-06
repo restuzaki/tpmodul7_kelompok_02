3ce07aa4fa6ed933ae4549259764bda1c00ffacc
Bagus Hardiyanto <bagushardiyanto03@gmail.com>
2025-04-07 00:02:44 +0700

    menambahkan class DataMahasiswa103022300025

---
+++ tpmodul7_kelompok_02/DataMahasiswa103022300025.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+public class DataMahasiswa103022300025
+{
+    public string nama { get; set; }
+    public string nim { get; set; }
+    public string fakultas { get; set; }
+
+    public static void ReadJSON()
+    {
+        string json = File.ReadAllText(@"../../../tp7_1_103022300025.json");
+        var mhs = JsonConvert.DeserializeObject<DataMahasiswa103022300025>(json);
+
+        Console.WriteLine($"Nama {mhs.nama} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+    }
+}
