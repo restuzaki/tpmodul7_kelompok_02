407c99ede8406f7e5289fdc99866cf20614f6eb6
fathanPrayoga <fathan.wicaksono@gmail.com>
2025-04-06 19:31:00 +0700

    menambahkan class DataMahasiswa103022330070

---
+++ tpmodul7_kelompok_02/DataMahasiswa103022330070.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_02
+{
+   
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; } 
+        public string fakultas { get; set; }
+    }
+
+    class DataMahasiswa103022330070
+    {
+        public void ReadJSON()
+        {
+            string filePath = "tp7_1_103022330070.json";
+
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                Mahasiswa data = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+                Console.WriteLine($"Nama {data.nama.depan} {data.nama.belakang} dengan nim {data.nim} dari fakultas {data.fakultas}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Terjadi kesalahan saat membaca file JSON: " + ex.Message);
+            }
+        }
+    }
+}
--- tpmodul7_kelompok_02/Program.cs
+++ tpmodul7_kelompok_02/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace tpmodul7_kelompok_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103022330070 dataMhs = new DataMahasiswa103022330070();
+            dataMhs.ReadJSON();
+            Console.ReadKey();
+        }
+    }
+}
+
